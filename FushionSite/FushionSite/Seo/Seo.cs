93389c0f0237b6daf9edcdb53b4019030afc6216
Ersin Tarhan <ersintarhan@gmail.com>
2017-06-14 05:40:09 +0300

    daha garanti slug oluşturmak için
    extension metodlar her zaman daha kullanışlı bence
    link.ToSeo() gibi.

--- FushionSite/FushionSite/Seo/Seo.cs
+++ FushionSite/FushionSite/Seo/Seo.cs
@@ -24,6 +24,27 @@ namespace Seo
 
             return trans;
         }
+        
+        
+         public static string ToSeo(this string phrase)
+        {
+            var str = phrase.RemoveAccent().ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+        
+        public static string RemoveAccent(this string txt)
+        {
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return Encoding.ASCII.GetString(bytes);
+        }
+        
 
         static Seo()
         {
